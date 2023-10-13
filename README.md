d9750513dcbfc6ebf7e272bcbf0514594d479c9c
pramodlodhi <138444308+pramodlodhi@users.noreply.github.com>
2023-10-13 17:51:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# online-food-order
+# OmniFood-Providing single platform for customers’ orders and restaurants.
+https://shashvat53.github.io/food-ordering-website/
---
+++ st.js
@@ -0,0 +1,26 @@
+//arthmetic operator
+
+/*var a=10;
+var b=5;
+console.log("the value of a+b is",a+b);
+console.log("the value of a*b is",a*b);
+console.log("the value of a-b is",a-b);
+console.log("the value of a/b is",a/b);
+*/
+
+//Assignment operator
+/*var c=a;
+c *=2;
+c +=2;
+c -=2;
+c /=2;
+console.log(c)
+*/
+//comparison operator
+/*var x=3;
+var y=5;
+console.log(x==y);
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+console.log(x=<y);*/
